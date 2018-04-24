28383d927f6493712aeee6b39324497b1da5eb64
pprabesh <prabes.pathak@gmail.com>
2018-04-24 18:18:19 +0545

    Add styles for province boxes and add sample data format

--- src/views/Landing/index.tsx
+++ src/views/Landing/index.tsx
@@ -16,36 +16,76 @@ interface Props {}
 export default class Landing extends React.PureComponent {
     provinces: number[];
     provincesData: {};
+    defaultProvince: object;
 
     constructor(props: Props) {
         super(props);
 
         this.provinces = [1, 2, 3, 4, 5, 6, 7];
+        this.defaultProvince = {};
         this.provincesData = {
-            1: 'Population',
-            2: 'Population',
-            3: 'Population',
+            1:  {
+                id: 1,
+                name: 'Province 1',
+                totalPopulation: 5404145,
+                povertyRate: 24.3,
+                noOfActiveProjects: 7,
+                totalBudget: 3434345225,
+            },
+            2: {},
+            3: {},
+            4: {},
+            5: {},
+            6: {
+                id: 6,
+                name: 'Province 6',
+                totalPopulation: 5404145,
+                povertyRate: 24.3,
+                noOfActiveProjects: 7,
+                totalBudget: 3434345225,
+            },
+            7: {},
         };
     }
 
-    getLinkForProvince = (provinceId: number) => (
-        reverseRoute(pathNames.province, { provinceId })
+    getLinkForProvince = (id: number) => (
+        reverseRoute(pathNames.province, { provinceId: id })
     )
 
-    renderProvinces = (provinceId: number) => (
-       <Link
-            key={provinceId}
-            to={this.getLinkForProvince(provinceId)}
-            className={styles.province}
-       >
-            <div>
-                Province {provinceId}
-            </div>
-            <div>
-                {this.provincesData[provinceId]}
-            </div>
-       </Link>
-    )
+    renderProvinces = (id: number) => {
+        const {
+            name,
+            totalPopulation,
+            noOfActiveProjects,
+            totalBudget,
+            povertyRate,
+        } = this.provincesData[id] || this.defaultProvince;
+
+        return (
+           <Link
+                key={id}
+                to={this.getLinkForProvince(id)}
+                className={styles.province}
+           >
+                <h2 className={styles.name}>
+                   <p>{name}</p>
+                </h2>
+                <div className={styles.population}>
+                    <span>Total Population: {totalPopulation}</span>
+                </div>
+                <div className={styles.poverty}>
+                    <span>Poverty Rate: {povertyRate}</span>
+                </div>
+
+                <div className={styles.projects}>
+                    <span>Active DFID Projects: {noOfActiveProjects}</span>
+                </div>
+                <div className={styles.budget}>
+                    <span>Total Budget(FY 2017/18): {totalBudget}</span>
+                </div>
+           </Link>
+        );
+    }
 
     render() {
         return (
@@ -59,6 +99,18 @@ export default class Landing extends React.PureComponent {
                             this.renderProvinces(provinceId)
                         ))
                     }
+                </div>
+                <div className={styles.bottom}>
+                    <div className={styles.dfiddata}>
+                        DFID
+                    </div>
+                    <div className={styles.about}>
+                        About
+                    </div>
+                    <div className={styles.dataset}>
+                        Dataset
+                    </div>
+
                 </div>
             </div>
         );
